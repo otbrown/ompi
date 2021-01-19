0d69d7600218b6a1b863951abfcc6a6af1d158ed
Dan Holmes <d.holmes@epcc.ed.ac.uk>
2021-01-19 20:06:14 +0000

    Add trigger_byrequest code

--- ompi/mca/coll/libpnbc_osc/pnbc_osc_trigger_common.c
+++ ompi/mca/coll/libpnbc_osc/pnbc_osc_trigger_common.c
@@ -10,8 +10,6 @@ int triggered_all_byzero_int(FLAG_t *trigger, void *cbstate) {
   return !(*(int*)trigger);
 }
 
-int triggered_all_byrequest_flag(FLAG_t *trigger, void *cbstate);
-
 void reset_all_to_zero_int(FLAG_t *trigger, FLAG_t value) {
   *(int*)trigger = 0;
 }
