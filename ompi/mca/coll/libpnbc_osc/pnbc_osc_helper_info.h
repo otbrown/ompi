50c3c748f4a33571534b866701ae8c08c1c2dfe0
Dan Holmes <dholmes-epcc-ed-ac-uk@users.noreply.github.com>
2021-01-20 16:51:56 +0000

    Update ompi/mca/coll/libpnbc_osc/pnbc_osc_helper_info.h

--- ompi/mca/coll/libpnbc_osc/pnbc_osc_helper_info.h
+++ ompi/mca/coll/libpnbc_osc/pnbc_osc_helper_info.h
@@ -1,3 +1,24 @@
-// TODO: copyright info - this was copied from portals4 in this git repo
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil -*- */
+/*
+ * Copyright (c) 2011-2017 Sandia National Laboratories.  All rights reserved.
+ * Copyright (c) 2015-2018 Los Alamos National Security, LLC.  All rights
+ *                         reserved.
+ * Copyright (c) 2015-2017 Research Organization for Information Science
+ *                         and Technology (RIST). All rights reserved.
+ * Copyright (c) 2017      The University of Tennessee and The University
+ *                         of Tennessee Research Foundation.  All rights
+ *                         reserved.
+ * Copyright (c) 2016-2017 IBM Corporation. All rights reserved.
+ * Copyright (c) 2018      Amazon.com, Inc. or its affiliates.  All Rights reserved.
+ * Copyright (c) 2020      EPCC, The University of Edinburgh. All rights
+ *                         reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * Author(s): Daniel Holmes  EPCC, The University of Edinburgh
+ *
+ * $HEADER$
+*/
 
 bool check_config_value_equal(char *key, ompi_info_t *info, char *value);
